1e227d07a3262b280a08593b73c0dbf84d3600be
Khawm Mung <johnmungtawng5@gmail.com>
2024-12-16 20:11:44 -0600

    very basic not found page

---
+++ app/not-found.tsx
@@ -0,0 +1,15 @@
+import Link from "next/link";
+import { headers } from "next/headers";
+
+export default async function NotFound() {
+    
+
+    return (
+        <div>
+            <h2>Not Found</h2>
+            <p>
+                Go back to <Link href="/">home</Link>.
+            </p>
+        </div>
+    );
+}
